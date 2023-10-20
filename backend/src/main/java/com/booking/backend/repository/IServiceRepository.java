b50b9651f4f66b6c9b22f171d8394d63f1040e12
Agustin Bruno <sav_agus@hotmail.com>
2023-10-20 02:50:50 -0300

    "db conection"

--- backend/src/main/java/com/booking/backend/repository/IServiceRepository.java
+++ backend/src/main/java/com/booking/backend/repository/IServiceRepository.java
@@ -1,11 +1,16 @@
 package com.booking.backend.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.booking.backend.models.Service;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IServiceRepository extends JpaRepository<Service, UUID> {
-  
+    @Query("Select s from Service s WHERE s.Name LIKE %:Name%")
+    public List<Service> findByName(@Param("Name") String Name);
+
 }
