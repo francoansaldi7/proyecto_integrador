b88fba561d168cc14b1d9fe274327e22319129a4
Deivid González <bienvenidoexperto@gmail.com>
2023-10-23 21:47:35 -0300

    fix bug of IServiceRepository

--- backend/src/main/java/com/booking/backend/repository/IServiceRepository.java
+++ backend/src/main/java/com/booking/backend/repository/IServiceRepository.java
@@ -5,5 +5,9 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.booking.backend.models.Services;
 
+
+public interface IServiceRepository extends JpaRepository<Services, UUID> {
+  
 }
