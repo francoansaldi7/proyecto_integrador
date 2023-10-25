9880c10f04bab33a62030ba36ca054efd6bd7afe
Franco Ansaldi <106043508+francoansaldi7@users.noreply.github.com>
2023-10-24 21:06:48 -0300

    modified card detail

--- frontend/src/pages/CardDetails.jsx
+++ frontend/src/pages/CardDetails.jsx
@@ -14,18 +14,23 @@ function CardDetails() {
     <div className="fixed top-64 bg-secondary w-[1200px] h-[500px] flex justify-between flex-row p-10 rounded-md ml-[200px] gap-10">
 
         <div className="mt-[70px]">
-            <img src={state.img} alt="main image" className="h-[250px] w-[500px]" />
-            <h1>{state.title}</h1>
+            <img src={state.img} alt="main image" className="h-[290px] w-[750px] rounded-md" />
+            <h1 className="flex justify-center mt-5 text-2xl">{state.title}</h1>
         </div>
 
-        <div className="flex flex-row flex-wrap gap-3 w-full">
-            <img className="w-[40%]" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
-            <img className="w-[40%]" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
-            <img className="w-[40%]" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
-            <img className="w-[40%]" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
+        <div className="flex flex-row flex-wrap justify-center gap-3 w-full rounded-md">
+            <img className="w-[48%] rounded-md" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
+            <img className="w-[48%] rounded-md" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
+            <img className="w-[48%] rounded-md" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
+            <img className="w-[48%] rounded-md" src="https://p0.pxfuel.com/preview/776/92/48/video-production-shoot-record.jpg" alt="test image" />
         </div>
 
-        <div className=""><Link to="/">x</Link></div>
+        <div className="flex flex-col justify-between">
+          <Link to="/">
+          <p className="ml-14">x</p>
+          </Link>
+          <button className="">Show more...</button>
+          </div>
     
     </div>
     </>
