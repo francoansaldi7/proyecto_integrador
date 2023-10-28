8c64d85c15b6697282b8219749ef243a7517246b
francoansaldi7 <fansaldi7@gmail.com>
2023-10-28 19:09:27 -0300

    modif(frontend): small modif to footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -6,18 +6,22 @@ function Footer() {
   
   return (
     <>
-    <div className="bg-primary h-60">
-      <p className="flex flex-row justify-center bg-primary p-5">Thank's for visiting GloCast!</p>
+    <div className="bg-primary text-white h-60">
+      <p className="flex flex-row justify-center text-xl bg-primary p-5">Thank's for visiting GloCast!</p>
 
       <div className="flex justify-center">
         <Link to="/"><img src="\logoTitle.png" alt="Test Logo" className="bg-primary h-14 w-14" /></Link>
       </div>
 
-      <div className="flex flex-row justify-center bg-primary mt-10 gap-7">
-          <Link to="/aboutUs"><p className="hover:text-violet-400">About Us</p></Link>
-          <Link to="/products&services"><p className="hover:text-violet-400">Products & Services</p></Link>
-          <Link to="/contact"><p className="hover:text-violet-400">Contact</p></Link>
+      <div className="flex flex-row justify-center text-lg bg-primary mt-6 gap-7">
+          <Link to="/aboutUs"><p className="hover:text-violet-300">About Us</p></Link>
+          <Link to="/products&services"><p className="hover:text-violet-300">Products & Services</p></Link>
+          <Link to="/contact"><p className="hover:text-violet-300">Contact</p></Link>
       </div>
+
+    <div className="mt-4">
+      <hr />
+    </div>
 
       <div>
         <p className='flex gap-2 mt-5 justify-center bg-primary'>
