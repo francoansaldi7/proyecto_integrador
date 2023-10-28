5f151659741b42015a20d3a9757f6f860ab5647a
AnaFerreiraPrz <ana.ferreirabna@gmail.com>
2023-10-28 01:20:00 -0300

    cleanup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,33 +5,20 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx}",
   ],
-  theme: {
-
-    // screens: {
-    //   'tablet': '640px',
-    //   // => @media (min-width: 640px) { ... }
-
-    //   'laptop': '1024px',
-    //   // => @media (min-width: 1024px) { ... }
-
-    //   'desktop': '1280px',
-    //   // => @media (min-width: 1280px) { ... }
-    // },
-    
+  theme: {    
     extend: {
       colors: {
-      primary: '#5465FF',
-      secondary: '#788BFF',
-      background: '#fff',
-      'background-dark': '#0A041C',
-      'primary-dark': '#1A1034',
-      'secondary-dark': '#241E4C',
+        primary: '#5465FF',
+        secondary: '#788BFF',
+        background: '#fff',
+        'background-dark': '#0A041C',
+        'primary-dark': '#1A1034',
+        'secondary-dark': '#241E4C',
+      },
+      backgroundImage: {
+        'hero-image': "url('https://th.bing.com/th/id/OIG._T3AfH4Bf128NEInXvKE?pid=ImgGn')",
+      }  
     },
-    backgroundImage: {
-      'hero-image': "url('https://th.bing.com/th/id/OIG._T3AfH4Bf128NEInXvKE?pid=ImgGn')",
-    }
-  
-  },
   },
   plugins: [],
 }
