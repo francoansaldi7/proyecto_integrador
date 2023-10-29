af21216c05a31dc8942850bfec81f784b08ecf7b
carmenzeballos <105959268+carmenzeballos@users.noreply.github.com>
2023-10-29 15:17:14 -0300

    feat(backend):Lógica del ReservationService

--- backend/src/main/java/com/booking/backend/models/Reservation.java
+++ backend/src/main/java/com/booking/backend/models/Reservation.java
@@ -9,9 +9,11 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class Reservation {
   @Id
   private UUID id;
