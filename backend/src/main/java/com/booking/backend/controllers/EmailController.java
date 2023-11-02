535d6d5f21aa9b9098bd88adb7a2329092d6960c
Deivid González <bienvenidoexperto@gmail.com>
2023-11-02 14:34:27 -0300

    feat(backend): added EmailService

---
+++ backend/src/main/java/com/booking/backend/controllers/EmailController.java
@@ -0,0 +1,22 @@
+package com.booking.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.booking.backend.services.impl.EmailService;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+  @Autowired
+  private EmailService emailService;
+  
+  @GetMapping
+  public String sendEmail() {
+    emailService.sendEmail("bienvenidoexperto@gmail.com", "Test Confirmation message", emailService.confirmationAccountMessage("https://www.google.com"));
+    return "Email sent successfully: " + emailService.sendEmail("bienvenidoexperto@gmail.com", "Test Confirmation message", emailService.confirmationAccountMessage("https://www.google.com"));
+  }
+}
