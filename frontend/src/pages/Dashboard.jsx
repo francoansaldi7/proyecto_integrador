41bcbb1a7dff761a2b4872e808fb189ee4351c82
Sofi Pereyra <sofi.pereyra23@gmail.com>
2023-11-07 16:36:46 -0300

    Add blocked dashboard alert

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,8 +3,21 @@ import { useState, useContext, useEffect } from "react";
 import ListOfServices from "../components/common/ListOfServices";
 import { GlobalContext } from "../contexts/globalContext";
 import ListOfUsers from "../components/common/ListOfUsers";
+import BlockedDashboard from "../components/common/BlockedDashboard";
 
 const Dashboard = () => {
+  const [isTabletOrSmaller, setIsTabletOrSmaller] = useState(window.innerWidth <= 768);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTabletOrSmaller(window.innerWidth <= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const {services} = useContext(GlobalContext);
   console.log(services);
 
@@ -12,20 +25,26 @@ const Dashboard = () => {
   const handleActiveCreateForm = () => {
     setActiveCreateForm(!activeCreateForm);
   };
-  
+
+
   return (
-      <div className="dark flex flex-col min-h-screen bg-gray-950 relative pt-24 items-center ">
-        <ListOfUsers></ListOfUsers>
-        {/* <button
-          className="bg-primary text-white px-4 z-10 py-2 h-10 rounded-lg"
-          onClick={handleActiveCreateForm}
-          type="button"
-        >
-          Add Service
-        </button> */}
-        <ListOfServices activeCreateForm={handleActiveCreateForm} />
-        {activeCreateForm && <CreateForm closeForm={handleActiveCreateForm} />}
-      </div>
+    <>
+      {isTabletOrSmaller ? <BlockedDashboard/>
+          :
+          <div className="dark flex flex-col min-h-screen bg-gray-950 relative pt-24 items-center ">
+              {/*<ListOfUsers></ListOfUsers>
+               <button
+                className="bg-primary text-white px-4 z-10 py-2 h-10 rounded-lg"
+                onClick={handleActiveCreateForm}
+                type="button"
+              >
+                Add Service
+              </button> */}
+              <ListOfServices activeCreateForm={handleActiveCreateForm} />
+              {activeCreateForm && <CreateForm closeForm={handleActiveCreateForm} />}
+          </div>}
+    </>
+      
   );
 };
 
