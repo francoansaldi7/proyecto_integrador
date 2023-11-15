320201a797ce7dd995a66ea4bb0f48180d6bdfc3
Deivid González <bienvenidoexperto@gmail.com>
2023-11-15 01:04:48 -0300

    feat(backend/frontend): Add CRUD for Categories, update create Service form for support characteristics, and multiple categories, add update form to the list of services

--- frontend/src/components/common/CreateFormCategories.jsx
+++ frontend/src/components/common/CreateFormCategories.jsx
@@ -1,95 +1,36 @@
 import { AiOutlineClose, AiOutlineCloudUpload } from "react-icons/ai";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import PropTypes from "prop-types";
-import Service from "../Service";
 import { GlobalContext } from "../../contexts/globalContext";
 import { toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
-const CreateForm = ({ closeForm }) => {
-  const [selectedImages, setSelectedImages] = useState([]);
+const CreateFormCategories = ({ closeForm }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
   const [description, setDescription] = useState("");
-  const [allCategories, setAllCategories] = useState([]);
-  const [category, setCategory] = useState([]);
-  const [categoriesSelected, setCategoriesSelected] = useState([]);
-  const [pricePerHour, setPricePerHour] = useState("");
-  const [serviceName, setServiceName] = useState("");
-  const [rating, setRating] = useState(1);
-  const [characteristics, setCharacteristics] = useState([
-    {
-      id: 1,
-      name: "Luces",
-    },
-    {
-      id: 2,
-      name: "Cámara",
-    },
-    {
-      id: 3,
-      name: "Estudio Fotográfico Completo",
-    },
-    {
-      id: 4,
-      name: "Tripodes",
-    },
-    {
-      id: 5,
-      name: "Edición de videos",
-    },
-    {
-      id: 6,
-      name: "Edición de fotografías",
-    },
-    {
-      id: 7,
-      name: "Camarografo Profesional",
-    },
-    {
-      id: 8,
-      name: "Album",
-    },
-  ]);
-  const [selectedCharacteristics, setSelectedCharacteristics] = useState([]);
+  const [categoryName, setCategoryName] = useState("");
   const [errors, setErrors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { saveService, getAllCategories } = useContext(GlobalContext);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const result = await getAllCategories();
-        if (result) {
-          setAllCategories(result);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  })
+  const { saveCategory } = useContext(GlobalContext);
   const handleImageUpload = (event) => {
-    const files = event.target.files;
-    const images = [...selectedImages];
-
-    for (const element of files) {
-      const file = element;
+    const file = event.target.files[0];
+    console.log(file);
       if (file.type.startsWith("image/")) {
         const imageUrl = URL.createObjectURL(file);
-        images.push({
+        setSelectedImage( {
           file,
           imageUrl,
+          fileName: file.name
         });
       }
     }
-    setSelectedImages(images);
-  };
 
-  const handleImageRemove = (index) => {
-    const newImages = [...selectedImages];
-    newImages.splice(index, 1);
-    setSelectedImages(newImages);
+
+  const handleImageRemove = () => {
+
+    setSelectedImage(null);
   };
 
   const handleDescription = (event) => {
@@ -100,87 +41,35 @@ const CreateForm = ({ closeForm }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setErrors([]);
-    let service;
-    try {
-      service = new Service(
-        serviceName,
-        category,
-        pricePerHour,
-        rating,
-        description,
-        selectedCharacteristics
-      );
-    } catch (error) {
-      setErrors((prev) => [...prev, error.message]);
-      return;
-    }
-    let savedService;
+    let savedCategory;
     setIsLoading(true);
     try {
-      savedService = await saveService(service, selectedImages);
+      savedCategory = await saveCategory({
+        category: {name: categoryName, description: description},
+        imageFile: selectedImage?.file,
+        fileName: selectedImage?.fileName
+      });
       setIsLoading(false);
       closeForm();
-      toast.success("¡Servicio creado exitosamente!");
+      toast.success("¡Categoría creada exitosamente!");
     } catch (error) {
       setIsLoading(false);
-      toast.error("Se produjo un error al crear el servicio.");
+      toast.error("Se produjo un error al crear la categoría.");
     }
-    console.log(savedService);
+    console.log(savedCategory);
   };
 
-  const handleCategoryChange = (event) => {
-    const newCategory = event.target.value;
-    console.log(newCategory);
-    let exists = false;
-    category.map((categoryIterable) => {
-      if (categoryIterable.id === newCategory) {
-        //sacar del array
-        setCategory(prev => prev.filter((category) => category.id !== newCategory));
-        exists = true;
-      }
-    })
-    if (!exists){
 
-      setCategory([...category, {id: newCategory}]);
-    }
-    console.log(category);
+
+  const handleCategoryName = (event) => {
+    const categoryName = event.target.value;
+    setCategoryName(categoryName);
   };
 
-  useEffect(() => {
-    let categories = [];
-    category.map((category) => {
-      categories.push(category.id)
-    })
-    setCategoriesSelected(categories);
-  }, [category])
 
-  const handleCharacteristics = (characteristic) => {
-    setSelectedCharacteristics((prev) => {
-      if (prev.includes(characteristic)) {
-        return prev.filter((characteristicInIterable) => characteristicInIterable !== characteristic);
-      } else {
-        return [...prev, characteristic];
-      }
-    })
-  }
-
-  const handlePricePerHour = (event) => {
-    const pricePerHour = event.target.value;
-    setPricePerHour(pricePerHour);
-  };
-
-  const handleServiceName = (event) => {
-    const serviceName = event.target.value;
-    setServiceName(serviceName);
-  };
-
-  const handleRating = (event) => {
-    const rating = event.target.value;
-    setRating(rating);
-  };
   return (
     <div
-      id="createProductModal"
+      id="createCategoryModal"
       className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 max-h-[calc(100vh-1rem)] md:h-full flex"
       aria-modal="true"
       role="dialog"
@@ -236,7 +125,7 @@ const CreateForm = ({ closeForm }) => {
           {/* <!-- Modal header --> */}
           <div className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Add Service
+              Añadir Categoría
             </h3>
             <button
               type="button"
@@ -252,83 +141,25 @@ const CreateForm = ({ closeForm }) => {
           {/* <!-- Modal body --> */}
           <form onSubmit={handleSubmit}>
             <div className="grid gap-4 mb-4 sm:grid-cols-2">
-              <div>
+              <div className="col-span-2">
                 <label
                   htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Nombre del Servicio
+                  Nombre
                 </label>
                 <input
                   type="text"
                   name="name"
                   id="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="Escriba el nombre del servicio"
+                  placeholder="Escriba el nombre de la categoría"
                   required=""
-                  onChange={handleServiceName}
-                  value={serviceName}
+                  onChange={handleCategoryName}
+                  value={categoryName}
                 />
               </div>
-              <div>
-                <label
-                  htmlFor="category"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Categoria
-                </label>
-                <select
-                  id="category"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
-                  required=""
-                  onChange={handleCategoryChange}
-                  value={categoriesSelected}
-                  multiple
-                >
-                  <option disabled>Select category</option>
-                  {allCategories.map((category) => (
-                      <option key={category.id} value={category.id}>{category.name}</option>
-                    
-                  ))}
-                </select>
-              </div>
-              <div>
-                <label
-                  htmlFor="rating"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Calificacion
-                </label>
-                <input
-                  type="number"
-                  name="rating"
-                  id="rating"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary-500"
-                  placeholder="Calificación del servicio"
-                  required=""
-                  onChange={handleRating}
-                  value={rating}
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="price"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Precio Por Hora
-                </label>
-                <input
-                  type="number"
-                  name="price"
-                  id="price"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                  placeholder="$2999"
-                  required=""
-                  onChange={handlePricePerHour}
-                  value={pricePerHour}
-                />
-              </div>
-
+              
               <div className="sm:col-span-2">
                 <label
                   htmlFor="description"
@@ -346,38 +177,11 @@ const CreateForm = ({ closeForm }) => {
                   value={description}
                 ></textarea>
               </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="description"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Características
-                </label>
-                <div className="flex gap-2 flex-wrap">
-                  {characteristics.map((characteristic) => (
-                    <div className="option" key={characteristic.id}>
-                      <input
-                        id={`characteristics-${characteristic.id}`}
-                        name="characteristics"
-                        value={characteristic.id}
-                        type="checkbox"
-                        className="m-2"
-                        required=""
-                        checked={selectedCharacteristics.includes(characteristic) }
-                        onChange={() => handleCharacteristics(characteristic)}
-                      ></input>
-                      <label htmlFor={`characteristics-${characteristic.id}`} className="dark:text-white">{characteristic.name}</label>
-                    </div>
-                  ))}
-                  
-
-                </div>
-              </div>
             </div>
 
             <div className="mb-4">
               <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Imágenes del producto
+              Imágene de la categoría
               </span>
               <div className="flex justify-center items-center w-full">
                 <label
@@ -398,25 +202,24 @@ const CreateForm = ({ closeForm }) => {
                     id="dropzone-file"
                     type="file"
                     className="hidden"
-                    multiple
+                    multiple={false}
                     accept="image/*"
                     onChange={handleImageUpload}
                   />
                 </label>
               </div>
               <div className="mt-4 grid grid-cols-4 gap-4">
-                {selectedImages.map((image, index) => (
-                  <div key={index} className="col-span-1 relative">
+               
+                  <div  className="col-span-1 relative">
                     <img
-                      src={image.imageUrl}
+                      src={selectedImage?.imageUrl}
                       className="cursor-pointer hover:shadow-md hover:shadow-slate-700 h-32 w-full object-cover rounded-md border-8 border-primary/30"
                     />
                     <AiOutlineClose
-                      onClick={() => handleImageRemove(index)}
+                      onClick={() => handleImageRemove()}
                       className="absolute top-2 right-2 cursor-pointer text-red-500 hover:text-red-700 "
                     />
                   </div>
-                ))}
               </div>
               <div className="errors flex col">
                 {errors.map((error, index) => (
@@ -431,7 +234,7 @@ const CreateForm = ({ closeForm }) => {
                 type="submit"
                 className="w-full sm:w-auto justify-center text-white inline-flex bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-secondary-dark dark:focus:ring-primary-dark relative"
               >
-                Agregar servicio
+                Agregar categoría
               </button>
               {/* <button className="w-full sm:w-auto text-white justify-center inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 <svg
@@ -459,8 +262,8 @@ const CreateForm = ({ closeForm }) => {
   );
 };
 
-CreateForm.propTypes = {
+CreateFormCategories.propTypes = {
   closeForm: PropTypes.func.isRequired,
 };
 
-export default CreateForm;
+export default CreateFormCategories;
